40dbc6ae12e251b39f98bf51500bffcc64d90d67
fsilvadev <fsilva.dev10@gmail.com>
2024-03-14 18:18:11 -0300

    first commit

---
+++ react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Produtos from './Produto';
+
+function App() {
+  return (
+    <div className="App">
+     <Produtos />
+    </div>
+  );
+}
+
+export default App;
